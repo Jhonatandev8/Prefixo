e13451ba7f75324e6d9088396d3fe2e3ceebb55d
Jhonatan de Paula <jhonatandev8@gmail.com>
2024-08-02 15:22:40 -0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Prefixo;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Solution solution= new Solution();
+        string[] strs = { "flower", "flow", "flight" };
+        string result = solution.LongestCommonPrefix(strs);
+        Console.WriteLine(result);
+    }
+}
